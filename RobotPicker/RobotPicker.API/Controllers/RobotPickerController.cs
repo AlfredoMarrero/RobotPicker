8baabb2463240aee4406f26c16db2ba271fa3446
AlfredoMarrero <a.e.marrero@hotmail.com>
2022-01-26 21:37:14 -0500

    Implement RobotPicker.API logic

---
+++ RobotPicker/RobotPicker.API/Controllers/RobotPickerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RobotPicker.Core.Models;
+using RobotPicker.Core.Services;
+
+namespace RobotPicker.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RobotPickerController : ControllerBase
+    {
+        private readonly IRobotService _robotService;
+
+        public RobotPickerController(IRobotService robotService) => _robotService = robotService;
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Load model)
+        {
+            if (model == null)
+            {
+                return BadRequest($"{nameof(Load)} can not be null");
+            }
+
+            var bestRobotToTrasportLoad = await _robotService.GetBestRobotToTransportLoadAsync(model);
+            return Ok(bestRobotToTrasportLoad);
+        }
+    }
+}
--- RobotPicker/RobotPicker.API/Program.cs
+++ RobotPicker/RobotPicker.API/Program.cs
@@ -1,3 +1,6 @@
+using RobotPicker.Core.Repositories;
+using RobotPicker.Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient();
+builder.Services.AddTransient<IRobotRepository, RobotRepository>();
+builder.Services.AddTransient<IRobotService, RobotService>();
 
 var app = builder.Build();
 
